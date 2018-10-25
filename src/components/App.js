dea917fcc2a747c5b05fc405b49551d42afa5ebd
Theo Okafor <theo.okafor@yahoo.com>
2018-10-25 06:08:18 +0100

    chore (setup): Configure React and Webpack - Create webpack configuration - Install React and webpack - Test run server
    [Delivers #161438816]

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+    <div>
+      <h1>StackOverflow-lite App</h1>
+    </div>
+    );
+  }
+}
+
+export default App;
